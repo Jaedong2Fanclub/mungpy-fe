8a286215520f1e9514f549c8fbf73aa3691e961c
yzooop <qkrduswn0404@gmail.com>
2024-11-23 11:57:09 +0900

    .

--- src/app/test/image/page.tsx
+++ src/app/test/image/page.tsx
@@ -1,4 +1,4 @@
-import ImageUpload from "../../../components/ImageUpload"; // 이미지 업로드 컴포넌트 불러오기
+import ImageUpload from "../../../components/ImageUpload"; // 이미지 업로드 컴포넌트 불러오기.
 
 export default function Page() {
     return (
